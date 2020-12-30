ff220cfe255e24b5022ab97d2031f79010b67c52
Sarfaraz4051 <k164051@nu.edu.pk>
2020-12-30 16:22:19 +0500

    Updated categories

--- main.js
+++ main.js
@@ -1,3 +1,56 @@
+let offers = [
+    {
+        "img_src": "images/offers/2.jpg",
+        "old_price": 1050,
+        "new_price": 950,
+        "title": "BUY Vital Tea Zip Pack 950 Gm GET Vital Cardamom Green Tea Bags Box 30 Pcs FREE"
+    },
+    {
+        "img_src": "images/offers/2.jpg",
+        "old_price": 1060,
+        "new_price": 861,
+        "title": "BUY 4 Nestle Cerelac 3 Fruits Baby Food 175 Gm GET Nestle Cerelac Bowl FREE"
+    },
+    {
+        "img_src": "images/offers/3.jpg",
+        "old_price": 1005,
+        "new_price": 729,
+        "title": "BUY Menu Burger Patties 1075 Gm GET Menu Chicken Nuggets 270 Gm FREE"
+    },
+    {
+        "img_src": "images/offers/4.jpg",
+        "old_price": 360,
+        "new_price": 330,
+        "title": "BUY Nestle Pure Life Water 1.5 Ltr X 6 Pcs GET 3 Nestle Everyday Sachet 15 Gm FREEE"
+    },
+    {
+        "img_src": "images/offers/5.jpg",
+        "old_price": 335,
+        "new_price": 315,
+        "title": "BUY Nescafe Classic Coffee 50 Gm GET 2 Nestle Everyday Sachet 15 Gm FREE"
+    },
+    {
+        "img_src": "images/offers/6.jpg",
+        "old_price": 399,
+        "new_price": 250,
+        "title": "BUY Dove Intense Repair Conditioner 180 Ml GET Lifebuoy Hand Sanitizer 60 Ml FREE"
+    },
+    {
+        "img_src": "images/offers/7.jpg",
+        "old_price": 160,
+        "new_price": 131,
+        "title": "BUY 2 Rafhan Orange Jelly 80 Gm GET Rafhan Mango Jelly 80 Gm FREEE"
+    },
+    {
+        "img_src": "images/offers/8.jpg",
+        "old_price": 405,
+        "new_price": 256,
+        "title": "BUY Fair & Lovely Moisturising Fairness Lotion 200 Ml GET Lifebuoy Hand Sanitizer 60 Ml FREE"
+    }
+  ]
+
+
+
 function showInfo(flag) {
         if (flag) {
             document.getElementById('sub-menu').style.display = "block"
